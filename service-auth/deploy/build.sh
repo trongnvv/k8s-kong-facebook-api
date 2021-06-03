517b375fbbd93dbc4bbd6854e4693b58d4533d8a
trongnv <trongnv138@gmail.com>
2021-06-04 02:49:30 +0700

    move out service

--- service-auth/deploy/build.sh
+++ service-auth/deploy/build.sh
@@ -3,5 +3,4 @@ DOCKER_TAG=trongnv138/k8s_facebook:service-auth-$VERSION
 cd ..
 docker build -t $DOCKER_TAG -f deploy/Dockerfile .
 docker push $DOCKER_TAG
-kubectl create configmap cm-auth --from-env-file=local.env
-kubectl apply -f deploy/k8s-exec.yaml
+#kubectl create configmap cm-auth --from-env-file=local.env
